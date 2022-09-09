6264cfc0ca71ce5a9dc6e4ed0051917b9b9f7d70
shivakharbanda <shivakharbanda11@gmail.com>
2022-09-09 15:07:45 +0530

    added grid size changing functionality. fixed bugs related to clear function

--- eas.js
+++ eas.js
@@ -4,13 +4,38 @@ container = document.querySelector('.etch-box');
 containHeight = 500;
 containerWidth = 500;
 area = containerWidth * containHeight;
+gridSize = 16;
 
+let rainbowMode = false;
 
-divMaker(4, container, containHeight)
+divMaker(gridSize)
 console.log(area)
 
+clearBtn = document.getElementById("clear");
+console.log(clearBtn);
 
-function divMaker(num, parent, containHeight) {
+clearBtn.addEventListener("click", clearGrid);
+
+
+gridBtn = document.getElementById("grid");
+
+gridBtn.addEventListener("click", (e) => {
+    gridSize = prompt("Enter grid size");
+    gridSize = Number(gridSize);
+    if (gridSize > 100) {
+        alert("invalid grid size. please enter a number between 1 and 100");
+        gridSize = 16;
+    }
+    removeGrid();
+    divMaker(Number(gridSize))
+
+
+})
+
+function divMaker(num) {
+    containHeight = 500;
+    parent = document.querySelector('.etch-box');
+
     for (let i = 0; i < num * num; i ++) {
         const div = document.createElement('div');
         parent.appendChild(div);
@@ -21,15 +46,42 @@ function divMaker(num, parent, containHeight) {
         div.style.width = `${boxHeight}px`
 
     }
+
+
+    let boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box => box.addEventListener("mouseover", (e) => {
+        boxy = e.target;
+        let qury = `${boxy.id}`;
+        boxy = document.getElementById(qury);
+        boxy.style.backgroundColor = "aqua";
+        //console.log(boxy);
+    }));
 }
 
-let boxes = document.querySelectorAll('.box');
+function clearGrid() {
+    let boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.style.backgroundColor = "white");
+}
 
-boxes.forEach(box => box.addEventListener("click", (e) => {
-    boxy = e.target;
-    let qury = `${boxy.id}`;
-    boxy = document.getElementById(qury);
-    boxy.classList.add('colored');
-    console.log(boxy);
-}));
+function removeGrid() {
+    papa = document.querySelector(".etch-box");
 
+    while (papa.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function checkSize(size) {
+    if (Number(size) > 100 && Number(size) < 0) {
+        return "false";
+    } 
+}
+
+
+
+function randomColor() {
+    let x = 0;
+    let y = 0;
+    let z = 0;
+}
